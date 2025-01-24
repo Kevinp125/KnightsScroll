945ae49aae905fa48f3678b79e4dff5af29de0ee
Owen <oweng325@gmail.com>
2025-01-24 18:23:23 -0500

    Added error check to make sure user is in the system

--- backend/api/AddContact.php
+++ backend/api/AddContact.php
@@ -18,7 +18,35 @@
     } 
     else
     {
-	// insert into contact databse entries, as labeled below
+        // first check for if the user exists
+
+        // to do this, we grab ID from users table
+        $checkStmt = $conn->prepare("SELECT ID FROM Users WHERE ID = ?");
+        if (!$checkStmt) 
+        {
+            returnWithError($stmt->error);
+        }      
+
+        // bind userId parameter
+        $checkStmt->bind_param("s", $userId);
+        if (!$checkStmt->execute()) 
+        {
+            returnWithError($stmt->error);
+        }
+
+        $checkStmt->store_result();
+        
+        // see if it returned nothing
+        if ($checkStmt->num_rows === 0) 
+        {
+            // id does not exist in Users table
+            $checkStmt->close();
+            $conn->close();
+            returnWithError("User ID does not exist.");
+        }
+        $checkStmt->close();      	    
+        
+        // insert into contact databse entries, as labeled below
         $stmt = $conn->prepare("INSERT INTO Contacts (UserId, FirstName, LastName, Phone, Email) VALUES (?, ?, ?, ?, ?)");
         
         // bind my vars to database vars in this order
