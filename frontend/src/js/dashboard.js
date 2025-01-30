3b56ad18bb0d0f72a4ed954a624756f6d0607fbd
Kevin Pereda <kptwdo15@gmail.com>
2025-01-30 14:51:33 -0500

    Going back to last commit because that didnt fix it now it isnt getting updated at all.

--- frontend/src/js/dashboard.js
+++ frontend/src/js/dashboard.js
@@ -196,9 +196,6 @@ document.addEventListener("DOMContentLoaded", () => {
           }
         };
 
-        editContactForm.replaceWith(editContactForm.cloneNode(true)); 
-        editContactForm = document.getElementById("editContactForm"); // Get new form
-
         document.getElementById("editFirstName").value = contact.firstName; //set the value of the first name input to the first name of the contact so user can see what they are editing
         document.getElementById("editLastName").value = contact.lastName; //set the value of the last name input to the last name of the contact so user can see what they are editing
         document.getElementById("editContactEmail").value = contact.email; //set the value of the email input to the email of the contact so user can see what they are editing
